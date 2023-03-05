31ffbaad0eb589b932dc2ae8b2b60314b9cbed49
Sebastian Ocando <ocandovivas@gmail.com>
2023-03-04 21:18:27 -0300

    header completed

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function Menu(e){
     let main = document.querySelector('main');
     let list = document.querySelector('ul');
-    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[1px]') , list.classList.add('opacity-100') , main.classList.add('opacity-5')) :( e.name = "menu" ,list.classList.remove('top-[1px]') , list.classList.remove('opacity-100') , main.classList.remove('opacity-5'))
+    let menu = document.getElementById('menu')
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[39px]') , menu.classList.add('max-md:bg-sky-200') , list.classList.add('opacity-100') , 
+    list.classList.add('max-md:bg-sky-200') , main.classList.add('opacity-5'), list.classList.add('z-[100]')) :( e.name = "menu" ,list.classList.remove('top-[39px]') , list.classList.remove('opacity-100') , main.classList.remove('opacity-5') , lis.classList.remove('z-[-1]'))
 }
